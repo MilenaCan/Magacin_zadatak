9a15fcd83a47adfeea905e35050b6b7613e17f83
Milena Cancar <milenaacancar@gmail.com>
2024-06-13 10:29:01 +0200

    Dodavanje i prikaz proizvoda

---
+++ magacin/src/AddProduct.tsx
@@ -0,0 +1,140 @@
+import {
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
+interface AddProductDialogProps {
+  open: boolean;
+  onClose: () => void;
+}
+const AddProduct: React.FC<AddProductDialogProps> = ({ open, onClose }) => {
+  const [productName, setProductName] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+  const [weight, setWeight] = useState("");
+  const [serialNumber, setSerialNumber] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const product = {
+      productName,
+      quantity,
+      price,
+      weight,
+      serialNumber,
+    };
+    console.log(product);
+    const existingProducts = JSON.parse(
+      localStorage.getItem("products") || "[]"
+    );
+    existingProducts.push(product);
+    localStorage.setItem("products", JSON.stringify(existingProducts));
+    setProductName("");
+    setQuantity("");
+    setPrice("");
+    setWeight("");
+    setSerialNumber("");
+    onClose();
+  };
+  const handleClose = () => {
+    // Reset fields
+    setProductName("");
+    setQuantity("");
+    setPrice("");
+    setWeight("");
+    setSerialNumber("");
+
+    onClose();
+  };
+  return (
+    <Box>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: handleSubmit,
+        }}
+      >
+        <DialogTitle>Dodavanje proizvoda</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="productName"
+            name="productName"
+            label="Naziv proizvoda"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="quantity"
+            name="quantity"
+            label="Kolicina"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="price"
+            name="price"
+            label="Cijena"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="weight"
+            name="weight"
+            label="Tezina"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="serialNumber"
+            name="serialNumber"
+            label="Serijski broj"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={serialNumber}
+            onChange={(e) => setSerialNumber(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Otkazi</Button>
+          <Button type="submit">Dodaj</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+};
+export default AddProduct;
